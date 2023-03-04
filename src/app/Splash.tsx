c43ee3b250dbc9a17c7d1a5f211ddf2eab5ec912
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2023-03-04 13:04:47 -0500

    splash animation - working but still needs troubleshoot

--- src/app/Splash.tsx
+++ src/app/Splash.tsx
@@ -1,16 +1,66 @@
+'use client';
+
 import styles from './page.module.css'
 import { Inter } from '@next/font/google'
+import Script from 'next/script'
+import { Text, Box, keyframes } from '@chakra-ui/react'
+import * as React from 'react';
 
 const inter = Inter({ subsets: ['latin'] })
 
+export const fadeDown: string = keyframes`
+  from {
+    transform: translateY(-20px);
+    opacity: 0;
+  } to {
+    transform: translateY(0px);
+    opacity: 1;
+  }
+`;
+
 export default function Splash() {
+    const [name, setName] = React.useState<boolean>(false);
+    const fadeDownAnim: string = `${fadeDown} 500ms 20ms forwards`;
+
     return(
         <div className={styles.center}>
-            <div className={styles.centerText}>
-                <h2 className={inter.className}>
-                    / wu.kaˈɕɛ.vit͡ʂ /
-                </h2>
-            </div>
+            <Box
+                mb="0.5rem"
+                pos="absolute"
+                top="180"
+                zIndex={2}
+                onMouseOver={ () => setName(!name)}
+                onMouseOut = { () => setName(!name)}
+                animation={fadeDownAnim}
+                transition={"300ms ease-in-out"}
+            >
+                {name ?
+                    <Text
+                    //   animation={fadeDownAnim}
+                    //   transition={"300ms ease-in-out"}
+                      fontWeight={600}
+                      fontFamily={"var(--chakra-fonts-mono)"}
+                      fontSize='2xl'
+                    > 
+                        /. Lukasiewicz ./ 
+                    </Text> 
+                :
+                    <Text
+                    //   animation={fadeDownAnim}
+                    //   transition={"300ms ease-in-out"}
+                      fontWeight={600}
+                      fontFamily={"var(--chakra-fonts-mono)"}
+                      fontSize='2xl'
+                    >
+                        / wu.kaˈɕɛ.vit͡ʂ /
+                    </Text>
+                }
+                 
+
+                    
+            </Box>
+                
+
         </div>
     )
 }
