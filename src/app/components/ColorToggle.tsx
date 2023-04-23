25809230a1888f165b8db17cca11ab383b47fc61
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2023-04-22 21:47:05 -0400

    toggle color mode icon animation working- troubleshooting chakraUI conflicts

--- src/app/components/ColorToggle.tsx
+++ src/app/components/ColorToggle.tsx
@@ -10,11 +10,10 @@ export const ColorToggle = () => (
     <label htmlFor="lib" className={styles.lib}> 
                 
       <svg className={styles.svg} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-    
-          <path d="M8 20H12M16 20H12M12 20V11" stroke="#020405" strokeWidth="1" strokeLinecap="round"/> 
+          <path d="M8 20H12M16 20H12M12 20V11" stroke="#C1B971" strokeWidth="1" strokeLinecap="round"/> 
                                                                                       {/* id={styles.onoff} */}
-          <path d="M17 11V16" stroke="#020405" strokeWidth="1" strokeLinecap="round" className={styles.onoff} /> 
-          <path d="M15 4H9C6.23858 4 4 6.23858 4 9V9.5C4 10.3284 4.67157 11 5.5 11H18.5C19.3284 11 20 10.3284 20 9.5V9C20 6.23858 17.7614 4 15 4Z" stroke="#020405" strokeWidth="1"/>
+          <path d="M17 11V16" stroke="#C1B971" strokeWidth="1" strokeLinecap="round" className={styles.onoff} /> 
+          <path d="M15 4H9C6.23858 4 4 6.23858 4 9V9.5C4 10.3284 4.67157 11 5.5 11H18.5C19.3284 11 20 10.3284 20 9.5V9C20 6.23858 17.7614 4 15 4Z" stroke="#C1B971" strokeWidth="1"/>
                                                                                     {/* id={styles.shine} */}
           <path d="M3 21L5 12H19L21 21H3Z" fill="url(#paint0_linear_976_2597)" className={styles.shine} />
                                                                                                                                                                                       {/*  id={styles.lamp} */}
@@ -46,49 +45,49 @@ export const ColorToggle = () => (
                             
   </div>
 )
-export const ColorToggleDay = () => (
-  <div className={styles.lampMode}>
+// export const ColorToggleDay = () => (
+//   <div className={styles.lampMode}>
               
-    <input type="checkbox" className={styles.checkbox}/>
+//     <input type="checkbox" className={styles.checkbox}/>
 
-    {/* for="lib" */}
-    <label className={styles.lib}> 
+//     {/* for="lib" */}
+//     <label className={styles.lib}> 
                 
-      <svg className={styles.svg} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+//       <svg className={styles.svg} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
     
-          <path className={styles.path} d="M8 20H12M16 20H12M12 20V11" stroke="#020405" strokeWidth="1" strokeLinecap="round"/> 
-                                                                                      {/* id={styles.onoff} */}
-          <path d="M17 11V16" stroke="#020405" strokeWidth="1" strokeLinecap="round" className={styles.onoff} /> 
-          <path className={styles.path} d="M15 4H9C6.23858 4 4 6.23858 4 9V9.5C4 10.3284 4.67157 11 5.5 11H18.5C19.3284 11 20 10.3284 20 9.5V9C20 6.23858 17.7614 4 15 4Z" stroke="#020405" strokeWidth="1"/>
-                                                                                    {/* id={styles.shine} */}
-          <path d="M3 21L5 12H19L21 21H3Z" fill="url(#paint0_linear_976_2597)" className={styles.shine} />
-                                                                                                                                                                                      {/*  id={styles.lamp} */}
-          <path d="M9 3C5.68629 3 3 5.68629 3 9V9.5C3 10.8807 4.11929 12 5.5 12H18.5C19.8807 12 21 10.8807 21 9.5V9C21 5.68629 18.3137 3 15 3H9Z" fill="url(#paint1_radial_976_2597)" className={styles.lamp} />
+//           <path className={styles.path} d="M8 20H12M16 20H12M12 20V11" stroke="#020405" strokeWidth="1" strokeLinecap="round"/> 
+//                                                                                       {/* id={styles.onoff} */}
+//           <path d="M17 11V16" stroke="#020405" strokeWidth="1" strokeLinecap="round" className={styles.onoff} /> 
+//           <path className={styles.path} d="M15 4H9C6.23858 4 4 6.23858 4 9V9.5C4 10.3284 4.67157 11 5.5 11H18.5C19.3284 11 20 10.3284 20 9.5V9C20 6.23858 17.7614 4 15 4Z" stroke="#020405" strokeWidth="1"/>
+//                                                                                     {/* id={styles.shine} */}
+//           <path d="M3 21L5 12H19L21 21H3Z" fill="url(#paint0_linear_976_2597)" className={styles.shine} />
+//                                                                                                                                                                                       {/*  id={styles.lamp} */}
+//           <path d="M9 3C5.68629 3 3 5.68629 3 9V9.5C3 10.8807 4.11929 12 5.5 12H18.5C19.8807 12 21 10.8807 21 9.5V9C21 5.68629 18.3137 3 15 3H9Z" fill="url(#paint1_radial_976_2597)" className={styles.lamp} />
 
           
-          <defs>
+//           <defs>
                               
-            <linearGradient id="paint0_linear_976_2597" x1="12" y1="12" x2="12" y2="21" gradientUnits="userSpaceOnUse">
+//             <linearGradient id="paint0_linear_976_2597" x1="12" y1="12" x2="12" y2="21" gradientUnits="userSpaceOnUse">
           
-                <stop stopColor="#CAFFC7" stopOpacity="0.65"/>
+//                 <stop stopColor="#CAFFC7" stopOpacity="0.65"/>
                                   
-                <stop offset="1" stopColor="#3B3E43" stopOpacity="0"/>
+//                 <stop offset="1" stopColor="#3B3E43" stopOpacity="0"/>
                               
-            </linearGradient>
+//             </linearGradient>
           
-            <radialGradient id="paint1_radial_976_2597" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(12 5.8125) rotate(90) scale(6.1875 12.375)">
+//             <radialGradient id="paint1_radial_976_2597" cx="0" cy="0" r="1" gradientUnits="userSpaceOnUse" gradientTransform="translate(12 5.8125) rotate(90) scale(6.1875 12.375)">
               
-                <stop stopColor="#8CF385"/>
+//                 <stop stopColor="#8CF385"/>
                             
-                <stop offset="1" stopColor="#397239"/>
+//                 <stop offset="1" stopColor="#397239"/>
                 
-            </radialGradient>
+//             </radialGradient>
                             
-          </defs>
+//           </defs>
                 
-      </svg>
+//       </svg>
               
-    </label>
+//     </label>
                             
-  </div>
-)
+//   </div>
+// )
--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,22 +1,36 @@
 "use client";
 import { ColorToggle } from "../components/ColorToggle";
 
-// import { 
-//         Icon, 
-//         Center,
-//         Box,
-//         useColorMode, 
-//         useColorModeValue as uCMV,  
-//       } from "@chakra-ui/react"
-      // const { toggleColorMode } = useColorMode();
-      // const SwitchIcon = uCMV(ColorToggle, ColorToggleDay);//might have to export icons in <Icon as={tag}/> from Icons page
-      // const upDownAnim: string= `${upDown} .4s linear forwards`
+import { 
+        Button,
+        Center,
+        useColorMode,  
+      } from "@chakra-ui/react"
+
+// const SwitchIcon = uCMV(ColorToggle, ColorToggleDay);//might have to export icons in <Icon as={tag}/> from Icons page
+
 export default function Contact() {
+  const { colorMode, toggleColorMode } = useColorMode();
     return(
         <div>
             <h2>Contact me</h2>
+            {/* <Center 
+              color={"transparent"}
+              width={43}
+              minWidth={35}
+              onClick={toggleColorMode}
+              _groupHover={{ transform: "translateX(-35px)" }}
+              _hover={{ cursor: "pointer" }}
+              > */}
+                <Button
+                  color={"goldenrod"}
+                  width={38}
+                  onClick={toggleColorMode}>
 
-            
+                  <ColorToggle />
+                  {/* Toggle{colorMode == 'light' ? ' Light ' : ' Dark '} */}
+                </Button>
+            {/* </Center> */}
         </div>
     )
 }
